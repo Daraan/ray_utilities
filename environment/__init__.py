9b9183b3b362487a068e16c8836eb6c285a2cb3d
Daniel Sperber <github.blurry@9ox.net>
2025-01-21 17:01:46 +0100

    add factory function for env creation with nickname lookups

---
+++ environment/__init__.py
@@ -0,0 +1,12 @@
+import gymnasium as gym
+
+env_short_names = {
+    "lunar" : "LunarLander-v2",
+    "cart" : "CartPole-v1",
+    "cartpole" : "CartPole-v1",
+}
+
+def create_env(name: str, **kwargs) -> gym.Env:
+    if name in env_short_names:
+        return gym.make(env_short_names[name], **kwargs)
+    return gym.make(name, **kwargs)
