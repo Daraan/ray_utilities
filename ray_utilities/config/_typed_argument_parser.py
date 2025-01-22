fbc9636a319e1fa5111dd6ff1e0342bc801a2797
Daniel Sperber <github.blurry@9ox.net>
2025-01-22 18:34:23 +0100

    Add typed argument parsers for experiment setup

---
+++ ray_utilities/config/_typed_argument_parser.py
@@ -0,0 +1,111 @@
+from __future__ import annotations
+
+from typing import Optional
+from typing_extensions import Literal
+from tap import Tap
+
+
+class _DefaultSetupArgumentParser(Tap):
+    agent_type: str
+    """Agent Architecture"""
+
+    env_type: str = "cart"
+    """Environment to run on"""
+
+    episodes: int = 1000
+    """How many episodes"""
+
+    seed: int | None = None
+    test: bool = False
+
+    extra: Optional[list[str]] = None
+
+    def configure(self) -> None:
+        # Short hand args
+        super().configure()
+        self.add_argument("-a", "--agent_type")
+        self.add_argument("-env", "--env_type")
+        self.add_argument("-e", "--episodes")
+        self.add_argument("-s", "--seed", default=None, type=int)
+
+
+class DefaultResssourceArgParser(Tap):
+    num_jobs: int = 5
+    """Trials to run in parallel"""
+
+    gpu: bool = False
+
+    parallel: bool = False
+    """Use multiple CPUs per worker"""
+
+    not_parallel: bool = False
+    """
+    Do not run multiple models in parallel, i.e. the Tuner will execute one job only.
+    This is similar to num_jobs=1, but one might skip the Tuner setup.
+    """
+
+    def configure(self) -> None:
+        super().configure()
+        self.add_argument("-J", "--num_jobs")
+        self.add_argument("-gpu", "--gpu")
+        self.add_argument("-mp", "--parallel")
+        self.add_argument("-np", "--not_parallel")
+
+
+class DefaultEnvironmentArgParser(Tap):
+    render_mode: Optional[Literal["human", "rgb_array", "ansi"]] = None
+    """Render mode"""
+
+    def configure(self) -> None:
+        super().configure()
+        self.add_argument(
+            "--render_mode",
+            "-render",
+            nargs="?",
+            const="rgb_array",
+            type=str,
+            default=None,
+            choices=["human", "rgb_array", "ansi"],
+        )
+
+
+class DefaultLoggingArgParser(Tap):
+    log_level: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"] = "INFO"
+    wandb: bool = False
+    comet: Literal["offline", "offline+upload", "0", "1", "False", "off", "on"] = "off"
+    comment: Optional[str] = None
+    tags: list[str] = []
+
+    def configure(self) -> None:
+        super().configure()
+        self.add_argument("--log_level")
+        self.add_argument("--wandb", "-wb")
+        self.add_argument(
+            "--comet",
+            nargs="?",
+            const="1",
+            default="off",
+            choices=["offline", "offline+upload", "0", "1", "False", "off", "on"],
+            type=str,
+        )
+        self.add_argument("--comment", "-c", type=str, default=None)
+        self.add_argument("--tags", nargs="+", default=[])
+
+
+class DefaultExtraArgs(Tap):
+    extra: Optional[list[str]] = None
+
+    def configure(self) -> None:
+        super().configure()
+        self.add_argument("--extra", help="extra arguments", nargs="+")
+
+
+class DefaultArgumentParser(
+    _DefaultSetupArgumentParser,
+    DefaultResssourceArgParser,
+    DefaultEnvironmentArgParser,
+    DefaultLoggingArgParser,
+    DefaultExtraArgs,
+):
+    def configure(self) -> None:
+        return super().configure()
--- ray_utilities/config/experiment_base.py
+++ ray_utilities/config/experiment_base.py
@@ -2,13 +2,26 @@ from __future__ import annotations
 
 # pyright: enableExperimentalFeatures=true
 
-import argparse
 from abc import ABC, abstractmethod
-from typing import TYPE_CHECKING, Any, Callable, Literal, Optional, Sequence, TypedDict
-from typing_extensions import TypeForm
+from typing import TYPE_CHECKING, Any, Callable, Sequence, TypedDict, Literal, overload
+
+from ._typed_argument_parser import DefaultArgumentParser
+
+__all__ = [
+    "DefaultArgumentParser",
+    "ExperimentSetupBase",
+]
 
 if TYPE_CHECKING:
+    import argparse
     from ray.rllib.algorithms import AlgorithmConfig
+    from typing_extensions import TypeForm
+    from ray.rllib.core.rl_module.rl_module import RLModuleSpec
+
+
+class TrainableReturnData(TypedDict, total=False):
+    pass
+
 
 class ExperimentSetupBase(ABC):
     """
@@ -20,84 +33,30 @@ class ExperimentSetupBase(ABC):
     """
 
     def create_parser(self) -> argparse.ArgumentParser:
-        parser = argparse.ArgumentParser()
-        parser.add_argument("-a", "--agent_type", help="architecture of agent to run", type=str, default="ddt")
-        parser.add_argument("-env", "--env_type", help="environment to run on", type=str, default="cart")
-        parser.add_argument("-e", "--episodes", help="how many episodes", type=int, default=1000)
-        parser.add_argument("-s", "--seed", help="Seed", default=None, type=int)
-        parser.add_argument("--test", "--dry-run", help="Do not save any models", action="store_true", default=False)
-        self._add_resource_args(parser)
-        self._add_environment_args(parser)
-        self._add_logging_args(parser)
+        return DefaultArgumentParser()
 
-        parser.add_argument("--extra", help="extra arguments", nargs="+", choices=[])
-        return parser
+    @overload
+    def create_config(self, args: argparse.Namespace, *, return_module_spec: Literal[False]) -> AlgorithmConfig: ...
 
-    def _add_resource_args(self, parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
-        parser.add_argument("-J", "--num_jobs", help="Amount of jobs the Tuner does start", default=5, type=int)
-        parser.add_argument("-gpu", "--gpu", help="run on GPU?", action="store_true")
-        parser.add_argument(
-            "-mp",
-            "--parallel",
-            help="Use multiple CPUs per worker",
-            action="store_true",
-            default=False,
-        )
-        parser.add_argument(
-            "-np",
-            "--not_parallel",
-            help="Do not run multiple models in parallel, i.e. the Tuner will execute one job only. "
-            "This is equivalent to num_jobs=1",
-            action="store_true",
-            default=False,
-        )
-        return parser
-
-    def _add_environment_args(self, parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
-        parser.add_argument(
-            "--render_mode",
-            "--render",
-            nargs="?",
-            const="rgb_array",
-            help="Render mode",
-            type=str,
-            default=None,
-            choices=["human", "rgb_array", "ansi"],
-        )
-        return parser
-
-    def _add_logging_args(self, parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
-        parser.add_argument(
-            "--log_level",
-            help="Set the log level",
-            type=str,
-            default="INFO",
-            choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
-        )
-        parser.add_argument("--wandb", "-wb", help="Log to WandB", action="store_true", default=False)
-        parser.add_argument(
-            "--comet",
-            nargs="?",
-            help="Log to Comet",
-            const="1",
-            default="off",
-            choices=["offline", "offline+upload", "0", "1", "False", "off", "on"],
-            type=str,
-        )
-        parser.add_argument("--comment", "-c", help="Add comment to this run", type=str, default="")
-        parser.add_argument(
-            "--tags", nargs="+", help="Add tags to this run to be used with wandb and comet", default=()
-        )
-        return parser
+    @overload
+    def create_config(
+        self, args: argparse.Namespace, *, return_module_spec: Literal[True] = True
+    ) -> tuple[AlgorithmConfig, RLModuleSpec]: ...
 
     @abstractmethod
-    def create_config(self, args: argparse.Namespace) -> AlgorithmConfig: ...
+    def create_config(
+        self, args: argparse.Namespace, *, return_module_spec: bool = True
+    ) -> tuple[AlgorithmConfig, RLModuleSpec] | AlgorithmConfig: ...
 
     @abstractmethod
-    def trainable_from_config(self, config: AlgorithmConfig) -> Callable[[dict[str, Any]], dict[str, Any]]: ...
+    def trainable_from_config(
+        self, *, args: argparse.Namespace | DefaultArgumentParser, config: AlgorithmConfig
+    ) -> Callable[[dict[str, Any]], TrainableReturnData]: ...
 
-    @property
-    @abstractmethod
-    def trainable_return_type(self) -> TypeForm[type[TypedDict]] | Sequence[str] | dict[str, Any]:
+    # @property
+    # @abstractmethod
+    def _trainable_return_type(
+        self,
+    ) -> TypeForm[type[TypedDict]] | Sequence[str] | dict[str, Any] | TrainableReturnData:
         """Keys or a TypedDict of the return type of the trainable function."""
         ...
