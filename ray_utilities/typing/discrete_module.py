6cfedabe930b7ea710b1669cfef66552332a0f40
Daniel Sperber <github.blurry@9ox.net>
2025-03-17 17:32:40 +0100

    renaming: interface names; add TFModule

--- ray_utilities/typing/discrete_module.py
+++ ray_utilities/typing/discrete_module.py
@@ -1,22 +1,29 @@
 from abc import ABC, abstractmethod
 from typing import TYPE_CHECKING, Protocol
 
+from ray.rllib.core.models.tf.base import TfModel
 from ray.rllib.core.models.torch.base import TorchModel  # noqa: F401  # type: ignore
 from typing_extensions import Self
 
 if TYPE_CHECKING:
-    import torch  # noqa: TC004
     from ray.rllib.core.rl_module.rl_module import RLModuleConfig
 
 
-class DiscreteModelBase(ABC):
-    """Model that implements create_discrete_copy for discrete evaluation"""
+class DiscreteModelABC(ABC):
+    """
+    Model that implements `create_discrete_copy` for discrete evaluation
+
+    TODO:
+        Otherwise implement a switch_mode; but might not work that well with logging.
+    """
 
     @abstractmethod
     def create_discrete_copy(self) -> Self: ...
 
 
-class _DiscreteTorchModelBase(DiscreteModelBase, torch.nn.Module if TYPE_CHECKING else TorchModel): ...
+class _DiscreteTorchModelBase(DiscreteModelABC, TorchModel): ...
+
+class _DiscreteTFModelBase(DiscreteModelABC, TfModel): ...
 
 
 class DiscreteModuleBase(Protocol):
@@ -32,15 +39,20 @@ class DiscreteModuleBase(Protocol):
     @abstractmethod
     def switch_mode(self, *, discrete: bool): ...
 
+class _TorchPPOModule(DiscreteModuleBase):
+    def __instance_members(self):
+        super().__instance_members()
+        self.pi: _DiscreteTorchModelBase
+        self.vf: _DiscreteTorchModelBase
 
-class DiscretePPOModule(DiscreteModuleBase):
-    if TYPE_CHECKING:
+class _TFPPOModule(DiscreteModuleBase):
+    def __instance_members(self):
+        super().__instance_members()
+        self.pi: _DiscreteTFModelBase
+        self.vf: _DiscreteTFModelBase
 
-        def __instance_members(self):
-            super().__instance_members()
-            self.pi: _DiscreteTorchModelBase
-            self.vf: _DiscreteTorchModelBase
 
+class DiscreteTorchPPOModule(_TorchPPOModule, DiscreteModuleBase):
     def switch_mode(self, *, discrete: bool):
         assert self.inference_only == self.config.inference_only
         if discrete and not self.is_discrete:
@@ -49,6 +61,23 @@ class DiscretePPOModule(DiscreteModuleBase):
             if not self.inference_only:  # vf is not used in inference and missing in later ray versions
                 self.vf = self.vf.create_discrete_copy()
                 self.vf.eval()
+            self.is_discrete = True
+        elif not discrete and self.is_discrete:
+            self.pi = self.pi
+            if not self.inference_only:
+                self.vf = self.vf
+            self.is_discrete = False
+
+
+class DiscreteTFPPOModule(_TFPPOModule, DiscreteModuleBase):
+    def switch_mode(self, *, discrete: bool):
+        assert self.inference_only == self.config.inference_only
+        if discrete and not self.is_discrete:
+            self.pi = self.pi.create_discrete_copy()
+            # self.pi.eval()
+            if not self.inference_only:  # vf is not used in inference and missing in later ray versions
+                self.vf = self.vf.create_discrete_copy()
+                # self.vf.eval()
             self.is_discrete = True
         elif not discrete and self.is_discrete:
             self.pi = self.pi
