898a6d5de0f8c45a172af81f5a296908cb0b48e9
Daniel Sperber <github.blurry@9ox.net>
2025-07-24 12:14:13 +0200

    Version 0.1.0 - metric restore working

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 # TODO: make some installations optional
 setup(
     name=r"ray_utilities",
-    version="0.0.11",
+    version="0.1.0",
     package_dir={"ray_utilities": "ray_utilities"},
     packages=find_packages(),
     include=["ray_utilities*"],
