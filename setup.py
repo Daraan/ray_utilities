eb997efe6a3a76f6b70176cef8880a4b0ecf9344
Daniel Sperber <github.blurry@9ox.net>
2025-08-15 16:25:32 +0200

    Remove version from setup.py -> pyproject.toml extend fields

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import find_packages, setup
 # TODO: make some installations optional
 setup(
     name=r"ray_utilities",
-    version="0.1.0",
     package_dir={"ray_utilities": "ray_utilities"},
     packages=find_packages(),
     include=["ray_utilities*"],
