e943d53ae22218317363dc0c17555bea1d33cd71
Daniel Sperber <github.blurry@9ox.net>
2025-08-18 11:30:42 +0200

    Revert "Add dynamic versioning for rc versions."
    This reverts commit 29a3901cdf110dcdd361247085aeabdcc2a8e616.

--- setup.py
+++ setup.py
@@ -1,20 +1,8 @@
 from setuptools import find_packages, setup
-import os
-import time
 
-version: str = "0.2.2"
-if "DEV_VERSION" in os.environ and "RC_VERSION" in os.environ:
-    raise ValueError("Cannot set both DEV_VERSION and RC_VERSION at the same time.")
-if "DEV_VERSION" in os.environ:
-    version += f".dev{int(time.time())}"  # Append a timestamp for development versions
-elif "RC_VERSION" in os.environ:
-    version += f".rc{int(time.time())}"
-
-
-# TODO: make some installations optional -> todo in pyproject.toml
+# TODO: make some installations optional
 setup(
     name=r"ray_utilities",
-    version=version,
     package_dir={"ray_utilities": "ray_utilities"},
     packages=find_packages(),
     include=["ray_utilities*"],
